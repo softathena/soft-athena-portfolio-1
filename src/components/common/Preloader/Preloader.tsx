fab48b424f63ae62f22a65270e9c5f85c73a5bb1
dev-mahbub <mahbub.devs@gmail.com>
2026-01-27 15:40:46 +0600

    updated portfolio

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -1,40 +1,14 @@
-// import React from 'react';
-
-// const Preloader = () => {
-//     return (
-//         <>
-//             <div id="preloader" className="sk-three-bounce">
-//                 <div className="sk-child sk-bounce1"></div>
-//                 <div className="sk-child sk-bounce2"></div>
-//                 <div className="sk-child sk-bounce3"></div>
-//             </div>
-//         </>
-//     );
-// };
-
-// export default Preloader;
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const Preloader = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (!loading) return null;
-
   return (
-    <div id="preloader" className="sk-three-bounce">
-      <div className="sk-child sk-bounce1"></div>
-      <div className="sk-child sk-bounce2"></div>
-      <div className="sk-child sk-bounce3"></div>
-    </div>
+    <>
+      <div id="preloader" className="sk-three-bounce">
+        <div className="sk-child sk-bounce1"></div>
+        <div className="sk-child sk-bounce2"></div>
+        <div className="sk-child sk-bounce3"></div>
+      </div>
+    </>
   );
 };
 
